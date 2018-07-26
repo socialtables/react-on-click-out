581cc7518102c03e0128da41031993a451e5b3d2
Brian Hong <brian.hong@socialtables.com>
2018-07-26 12:05:19 -0400

    Prevent drag from triggering single touch fxn call

--- src/index.js
+++ src/index.js
@@ -9,11 +9,18 @@ function parentClassIncludes(target, className) {
 	return false;
 }
 
+const DRAG_THRESHOLD = 10;
 
 export default class OnClickOut extends Component {
 	constructor() {
 		super();
 		this.onClick = this.onClick.bind(this);
+		this.onTouchStart = this.onTouchStart.bind(this);
+		this.onTouchMove = this.onTouchMove.bind(this);
+		this.onTouchEnd = this.onTouchEnd.bind(this);
+		this.onTouchCancel = this.onTouchCancel.bind(this);
+		this.startListenToTouchEvents = this.startListenToTouchEvents.bind(this);
+		this.stopListenToTouchEvents = this.stopListenToTouchEvents.bind(this);
 	}
 	onClick(e) {
 		const hasIgnoredClasses = this.props.ignoredClasses.some(name => parentClassIncludes(e.target, name));
@@ -24,14 +31,65 @@ export default class OnClickOut extends Component {
 		}
 	}
 
-	componentDidMount() {
-		document.addEventListener("click", this.onClick);
-		document.addEventListener("touchstart", this.onClick);
+	onTouchStart({touches: [touch]}) {
+		this.userDragging = false;
+		this.startX = touch.clientX;
+		this.startY = touch.clientY;
 	}
 
+	onTouchMove({touches: [touch]}) {
+		if (
+			(Math.abs(touch.clientX - this.startX) > DRAG_THRESHOLD) || 
+			(Math.abs(touch.clientY - this.startY) > DRAG_THRESHOLD)
+		) {
+			this.userDragging = true;
+		}
+	}
+
+	onTouchEnd(e) {
+		if (this.userDragging) {
+			return;
+		}
+
+		const hasIgnoredClasses = this.props.ignoredClasses.some(name => parentClassIncludes(e.target, name));
+		if (this.node && this.node.contains) {
+			if (!this.node.contains(e.target) && !hasIgnoredClasses) {
+				this.props.onClickOut();
+			}
+		}
+
+		this.startX = 0;
+		this.startY = 0;
+	}
+
+	onTouchCancel() {
+		this.userDragging = false;
+		this.startX = 0;
+		this.startY = 0;
+	}
+
+	startListenToTouchEvents() {
+		document.addEventListener("touchstart", this.onTouchStart);
+		document.addEventListener("touchmove", this.onTouchMove);
+		document.addEventListener("touchend", this.onTouchEnd);
+		document.addEventListener("touchcancel", this.onTouchCancel);
+	}
+
+	stopListenToTouchEvents() {
+		document.removeEventListener("touchstart", this.onTouchStart);
+		document.removeEventListener("touchmove", this.onTouchMove);
+		document.removeEventListener("touchend", this.onTouchEnd);
+		document.removeEventListener("touchcancel", this.onTouchCancel);
+	}
+
+	componentDidMount() {
+		document.addEventListener("click", this.onClick);
+		this.startListenToTouchEvents();
+	}
+	
 	componentWillUnmount() {
 		document.removeEventListener("click", this.onClick);
-		document.removeEventListener("touchstart", this.onClick);
+		this.stopListenToTouchEvents();
 	}
 
 	render() {
